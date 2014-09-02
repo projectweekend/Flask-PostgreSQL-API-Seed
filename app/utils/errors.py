6929ac2c8f23adc2ed53f430a51856853ab72d9f
Brian Hines <brian@projectweekend.net>
2014-09-02 13:33:51 -0500

    Add auth error

--- app/utils/errors.py
+++ app/utils/errors.py
@@ -1,3 +1,4 @@
 # Define custom error messages here
 
 EMAIL_IN_USE = ({'message': 'User with that email already exists'}, 409)
+UNAUTHORIZED = ({'message': 'Authentication required to access this resource'}, 401)

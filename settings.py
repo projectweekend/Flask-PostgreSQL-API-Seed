07389fe3f817555dcd137af5c843f4a6bcb5b567
Brian Hines <brian@projectweekend.net>
2014-09-03 07:39:00 -0500

    Clean up settings.py

--- settings.py
+++ settings.py
@@ -1,18 +1,14 @@
 import os
 
-DEBUG = os.getenv('DEBUG', False)
-
-PORT = os.getenv('PORT')
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
+DATABASE_CONNECT_OPTIONS = {}
+DEBUG = os.getenv('DEBUG', False)
+PORT = os.getenv('PORT')
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',
     'postgres://docker:docker@{0}/docker'.format(os.getenv('DB_1_PORT_5432_TCP_ADDR')))
-DATABASE_CONNECT_OPTIONS = {}
-
+SECRET_KEY = os.getenv('SECRET_KEY', None)
 THREADS_PER_PAGE = 2
 
-CSRF_ENABLED = False
 
-SECRET_KEY = os.getenv('SECRET_KEY', None)
 assert SECRET_KEY

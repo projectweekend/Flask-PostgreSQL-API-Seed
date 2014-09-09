b5cde94396458c67e74ef407b2ab24a417ea4e76
Brian Hines <brian@projectweekend.net>
2014-09-09 05:44:19 -0500

    Hook up URL routes

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,6 @@ from users import api as users_api
 
 rest_api.add_resource(users_api.UserAPI, '/api/v1/user')
 rest_api.add_resource(users_api.AuthenticationAPI, '/api/v1/authenticate')
+rest_api.add_resource(users_api.PasswordResetRequestAPI, '/api/v1/password-reset/request')
+rest_api.add_resource(users_api.PasswordResetConfirmAPI, '/api/v1/password-reset/confirm')
 rest_api.add_resource(users_api.AdminOnlyAPI, '/api/v1/admin')

bd15cdefe648d18030d19b37d532deee12f2f5b2
Brian Hines <brian@projectweekend.net>
2014-09-09 05:46:49 -0500

    Typo

--- app/utils/errors.py
+++ app/utils/errors.py
@@ -1,5 +1,5 @@
 # Define custom error messages here
 
 EMAIL_IN_USE = ({'message': 'User with that email already exists'}, 409)
-UNAUTHORIZED = ({'message': 'Authentication ie required to access this resource'}, 401)
+UNAUTHORIZED = ({'message': 'Authentication is required to access this resource'}, 401)
 FORBIDDEN = ({'message': 'Access to this resource is forbidden'}, 403)

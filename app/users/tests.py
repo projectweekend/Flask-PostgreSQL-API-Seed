d1218f6385cc028d09c49c3b402a0c84db2a1805
Brian Hines <brian@projectweekend.net>
2014-09-11 05:35:29 -0500

    More tests

--- app/users/tests.py
+++ app/users/tests.py
@@ -1,7 +1,8 @@
 import json
 
-
+from app import db
 from app.utils.testing import ApiTestCase
+from app.users.models import PasswordReset
 
 
 class UserApiTest(ApiTestCase):
@@ -17,3 +18,49 @@ class UserApiTest(ApiTestCase):
 
         assert data['id'] == 1
         assert len(data['token']) > 10
+
+    def test_duplicate_registration(self):
+        response = self.app.post('/api/v1/user', data=self.user_data)
+        assert response.status_code == 201
+
+        response = self.app.post('/api/v1/user', data=self.user_data)
+        assert response.status_code == 409
+
+    def test_get_user(self):
+        response = self.app.post('/api/v1/user', data=self.user_data)
+        data = json.loads(response.data)
+
+        headers = {'Authorization': data['token']}
+
+        response = self.app.get('/api/v1/user', headers=headers)
+        data = json.loads(response.data)
+        assert data['email'] == 'something@email.com'
+
+    def test_password_reset(self):
+        response = self.app.post('/api/v1/user', data=self.user_data)
+        assert response.status_code == 201
+
+        response = self.app.post('/api/v1/password-reset/request', data={
+            'email': 'something@email.com'
+        })
+        assert response.status_code == 201
+
+        pw_reset = db.session.query(PasswordReset).first()
+
+        response = self.app.post('/api/v1/password-reset/confirm', data={
+            'code': "bad code",
+            'password': 'abc123'
+        })
+        assert response.status_code == 401
+
+        response = self.app.post('/api/v1/password-reset/confirm', data={
+            'code': pw_reset.code,
+            'password': 'abc123'
+        })
+        assert response.status_code == 200
+
+        response = self.app.post('/api/v1/authenticate', data={
+            'email': 'something@email.com',
+            'password': 'abc123'
+        })
+        assert response.status_code == 200
